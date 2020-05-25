e0a59a1f52576b1e1b9452b8388bdac9c77df69b
Michael Conour <michaelconour@gmail.com>
2020-05-24 20:48:08 -0700

    started timer function

--- main.js
+++ main.js
@@ -8,4 +8,28 @@ let workSeconds = document.getElementById('work-seconds');
 let breakMinutes = document.getElementById('break-minutes');
 let breakSeconds = document.getElementById('break-seconds');
 
+let startTime
 
+timer = () => {
+    // this is the work timer countdown
+    if (workSeconds.innerText != 0) {
+        workSeconds.innerText--;
+    } else if (workMinutes.innerText != 0 && workSeconds.innerText == 0) {
+        workSeconds.innerText = 59;
+        workMinutes.innerText--;
+    }
+
+    // this is the break timer countdown
+    if (workMinutes.innerText == 0 && workSeconds.innerText == 0) {
+        if (breakSeconds.innerText != 0) {
+            breakSeconds.innerText--;
+        } else if (breakMinutes.innerText != 0 && breakSeconds.innerText == 0) {
+            breakSeconds.innerText = 59;
+            breakMinutes.innerText--;
+        }
+    }
+    // this is the increment counter (cycles) that'll go up by 1 if both work and break timers completed
+    if (workMinutes.innerText == 0 && workSeconds == 0 && breakMinutes.innerText == 0 && breakSeconds.innerText == 0) {
+
+    }
+}
