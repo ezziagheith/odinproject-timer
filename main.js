4a4f833119c39c61aaf01753ed482d1ef9185589
Michael Conour <michaelconour@gmail.com>
2020-05-24 18:15:45 -0700

    started creating variables to get start, stop, and reset btns

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 let start = document.getElementById('start');
 let reset = document.getElementById('stop');
 let stop = document.getElementById('reset');
+
+let workMinutes = document.getElementById('work-minutes');
+let workSeconds = document.getElementById('work-seconds');
+
+let breakMinutes = document.getElementById('break-minutes');
+let breakSeconds = document.getElementById('break-seconds');
